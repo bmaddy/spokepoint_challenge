e6eb2f05670e4c4b32a56214ca7f69ad3a990ee7
Brian Maddy <brian@brianmaddy.com>
2014-09-17 21:11:15 -0500

    got cljs headless repl working

--- project.clj
+++ project.clj
@@ -5,13 +5,17 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [om "0.7.3"]]
+                 [om "0.7.3"]
+                 [sablono "0.2.22"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
   :hooks [leiningen.cljsbuild]
 
   :source-paths ["src"]
 
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]]}}
+  ;; (cemerick.piggieback/cljs-repl :repl-env (cemerick.austin/exec-env))
+  
   :cljsbuild { 
     :builds [{:id "spokepoint-challenge"
               :source-paths ["src"]
--- src/spokepoint_challenge/core.cljs
+++ src/spokepoint_challenge/core.cljs
@@ -1,8 +1,104 @@
 (ns spokepoint-challenge.core
-  (:require [om.core :as om :include-macros true]
+  (:require [clojure.string :as s]
+            [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
+
+(def raw-data "Outlet name
+First name
+Last name
+Pitch Angle
+Relevance
+Reach
+Replied?
+Business Insider
+Caroline
+Moss
+tech
+4
+3
+N
+TechCrunch
+Darrell
+Etherington
+tech
+4
+3
+N
+Mashable
+Samantha
+Murphy Kelly
+tech
+4
+3
+Y
+Mashable
+Fran
+Berkman
+tech
+4
+3
+N
+RetailMeNot
+Tricia
+Romano
+tech
+3
+1
+N
+CNBC
+Cadie
+Thompson
+boredom
+3
+4
+N
+New York Times
+Mike
+Hendricks
+tech
+3
+4
+Y
+SFGate
+Angela
+Swartz
+tech
+3
+3
+Y
+Gizmodo
+Anna Jane
+Grossman
+boredom
+3
+3
+Y
+LifeHacker
+Walter
+Glenn
+boredom
+4
+2
+N
+USA Today
+Edward C.
+Baig
+boredom
+2
+4
+Y
+")
+
+(def columns [:outlet-name :first-name :last-name :pitch-angle :relevance :reach :replied?])
+(def column-converters [str str str str #(.parseInt js/Number %) #(.parseInt js/Number %) {"Y" true "N" false}])
+
+(def data (->> (s/split raw-data #"\n")
+               (partition 7)
+               rest
+               (map (fn [values] (map #(%1 %2) column-converters values)))
+               (map #(zipmap columns %))))
 
 (def app-state (atom {:text "Hello world!"}))
 
